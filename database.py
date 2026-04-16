5435f607e000df38868d6637fd3a13f6dbc9a971
berNard <aggreybernard3@gmail.com>
2026-04-16 16:14:30 -0400

    fix database.py

--- database.py
+++ database.py
@@ -3,7 +3,6 @@ from databases import Database
 from sqlalchemy import create_engine, MetaData
 from dotenv import load_dotenv 
 
-
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -16,7 +15,3 @@ database = Database(DATABASE_URL, min_size=1, max_size=5)
 metadata = MetaData()
 engine = create_engine(SYNC_DATABASE_URL, connect_args={"connect_timeout": 10})
 
-print("DEBUG DB URL:", DATABASE_URL)
-
-metadata.drop_all(engine)
-metadata.create_all(engine)
